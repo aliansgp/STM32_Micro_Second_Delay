ed8d0fefa21b49018349690868beca42e7b1582a
ali abdollahian <aliansgp@gmail.com>
2023-01-12 16:10:04 +0330

    Write us_delay function
    Import tim.h library for htim1
    set CNT to zero
    wait for CNT to reach us(input) value

--- Core/Src/us_Delay.c
+++ Core/Src/us_Delay.c
@@ -4,5 +4,13 @@
  *  Created on: Jan 12, 2023
  *      Author: Ali
  */
+#include "tim.h"
 
+
+void us_delay(int us){
+	HAL_TIM_Base_Start(&htim1);
+	htim1.Instance->CNT=0;
+	while(htim1.Instance->CNT < us);
+
+}
 
